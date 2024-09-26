1c82f25fac5bcd15efd814dbc819e44e3878dee6
Ag Ibragimov <agzam.ibragimov@gmail.com>
2024-09-26 13:05:15 -0500

    pulse changes
    thanks to @andersjohansson https://github.com/agzam/.doom.d/issues/1

--- ag-themes-base16-ocean-theme.el
+++ ag-themes-base16-ocean-theme.el
@@ -162,7 +162,8 @@
                 (elfeed-search-title-face (:foreground ,base03))
                 (elfeed-search-unread-title-face (:foreground ,base04))
                 (elfeed-search-feed-face (:foreground ,base0A))
-                (elfeed-search-tag-face (:foreground ,base0B))))
+                (elfeed-search-tag-face (:foreground ,base0B))
+                (pulse-highlight-start-face (:background "orange1"))))
        (new-faces (append faces (ag-themes--modify-modeline-faces '(:height 0.85)))))
   (color-theme-set-faces 'ag-themes-base16-ocean
                          'base16-ocean
